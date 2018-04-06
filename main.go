fa8622fe3ad8fdb38e91c96c51c6c90a3929951f
HAOYUatHZ <haoyu@protonmail.com>
2018-04-06 13:27:40 +0800

    framework Alpha

--- main.go
+++ main.go
@@ -7,32 +7,38 @@ import(
 )
 
 var poolAddr = "stratum-btm.antpool.com:6666/"
+// var poolAddr = "https://api.typeform.com/forms"
+
 
 func main() {
-    fmt.Println(poolAddr)
-
     request := gorequest.New()
-    // resp, body, errs := request.Get(req_url).End()
 
-    // fmt.Println(string(json_dat))
-
-    // _, body, _ := request.Post("https://httpbin.org/post").
-    //     Send(string(json_dat)).
-    //     // Send(`{"data":1}`).
+    // resp, body, _ := request.Post(poolAddr).
+    //     Send(`{
+    //             "id": 1,
+    //             "jsonrpc": "2.0",
+    //             "method": "login",
+    //             "params": [
+    //                 "0xb85150eb365e7df0941f0cf08235f987ba91506a",//login
+    //                 "",//Pass
+    //                 "agent"//Agent
+    //             ]
+    //         }`).
     //     End()
+    // fmt.Println(resp)
+
 
     _, body, _ := request.Post(poolAddr).
         Send(`{
-                  "id": 1,
-                  "jsonrpc": "2.0",
-                  "method": "login",
-                  "params": [
-                        "0xb85150eb365e7df0941f0cf08235f987ba91506a",//login
-                        "",//Pass
-                        "agent"//Agent
-                    ]
+                "id": 1,
+                "jsonrpc": "2.0",
+                "method": "login",
+                "params": [
+                    "0xb85150eb365e7df0941f0cf08235f987ba91506a",//login
+                    "",//Pass
+                    "agent"//Agent
+                ]
             }`).
         End()
-
     fmt.Println(body)
 }
