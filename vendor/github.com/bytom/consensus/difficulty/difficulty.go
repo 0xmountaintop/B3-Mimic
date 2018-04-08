9a935e380172c34f7b2b76212a2adda0f6577793
HAOYUatHZ <haoyu@protonmail.com>
2018-04-08 11:20:18 +0800

    paused

--- vendor/github.com/bytom/consensus/difficulty/difficulty.go
+++ vendor/github.com/bytom/consensus/difficulty/difficulty.go
@@ -2,6 +2,7 @@ package difficulty
 
 import (
 	"math/big"
+	"fmt"
 
 	"github.com/bytom/consensus"
 	"github.com/bytom/mining/tensority"
@@ -97,6 +98,12 @@ func BigToCompact(n *big.Int) uint64 {
 // CheckProofOfWork checks whether the hash is vaild for a given difficulty.
 func CheckProofOfWork(hash, seed *bc.Hash, bits uint64) bool {
 	compareHash := tensority.Hash(hash, seed)
+
+	fmt.Println(HashToBig(compareHash), "vs", CompactToBig(bits))
+
+	// 73244236200006804896540967629562784336130572291957598975277747150438572030501
+	// 94847093308577653384185819518661050448069533085310344421636332765315072
+
 	return HashToBig(compareHash).Cmp(CompactToBig(bits)) <= 0
 }
 
